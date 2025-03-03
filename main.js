b7e51a456831ab167a2385fff20261b044c7cb82
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2025-03-03 18:26:29 +0300

    updated

--- main.js
+++ main.js
@@ -55,6 +55,23 @@ const getMean = (array) => {
     }
   }
 
+  const getMode = (array) => {
+    const counts = {};
+    array.forEach((el) => {
+      counts[el] = (counts[el] || 0) + 1;
+    })
+    if (new Set(Object.values(counts)).size === 1) {
+      return null;
+    }
+    const highest = Object.keys(counts).sort(
+      (a, b) => counts[b] - counts[a]
+    )[0];
+    const mode = Object.keys(counts).filter(
+      (el) => counts[el] === counts[highest]
+    );
+    return mode.join(", ");
+  }
+  
   const calculate = () => {
     const value = document.querySelector("#numbers").value;
     const array = value.split(/,\s*/g);
